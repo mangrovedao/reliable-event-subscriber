5ede4a17076699848e957f0b154f60beb21f1dde
Louis-Amas <amaslouis@protonmail.com>
2023-05-11 09:55:10 +0200

    fix: remove trailing zeroes getLogs

--- scripts/test-http-provider.ts
+++ scripts/test-http-provider.ts
@@ -2,8 +2,9 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 import { JsonRpcProvider } from "@ethersproject/providers";
-import { ReliableHttpProvider, enableLogging } from "../src";
+import { BlockManager, LogSubscriber, ReliableHttpProvider, enableLogging } from "../src";
 import { sleep } from "@mangrovedao/commonlib.js";
+import { Log } from "@ethersproject/providers";
 
 enableLogging();
 
@@ -31,16 +32,51 @@ const reliableProvider = new ReliableHttpProvider(
   }
 );
 
+class Subscriber extends LogSubscriber<any> {
+
+  /**
+   * initialize subscriber at block `block`.
+   */
+  public async initialize(
+    block: BlockManager.BlockWithoutParentHash
+  ): Promise<LogSubscriber.InitializeErrorOrBlock> {
+    return undefined;
+  }
+
+  /**
+   * handle log
+   */
+  async handleLog(log: Log): Promise<void> {
+    try {
+    } catch(e){
+      console.error(e);
+    }
+  }
+
+  /**
+   * rollback subscriber to block `block`
+   */
+  rollback(block: BlockManager.Block): void {
+
+  }
+}
+
 const main = async() => {
   console.log('initialize', rpcHttpUrl);
 
   const block = await provider.getBlock('latest');
+
+  const sub = new Subscriber();
 
   await reliableProvider.initialize({
     parentHash: block.parentHash,
     hash: block.hash,
     number: block.number,
   });
+  await reliableProvider.blockManager.subscribeToLogs({
+    address: '0x823701dD29451766d5B9eF6b54ef42642F545cd7',
+    topics: [],
+  }, sub);
 
   await sleep(1000000000);
 }
